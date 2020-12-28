33950911271ae810887e0b2abced24adaaccf2d1
이진우 <jinus7542@gmail.com>
2020-12-28 12:22:32 +0900

    rename.    http -> api.    ws -> broker.

--- 2.gw-api/sam.sh
+++ 2.gw-api/sam.sh
@@ -2,7 +2,7 @@
 
 source `dirname "../bash/$BASH_SOURCE"`/getopts.sh
 
-stack="gw-ws"
+stack="gw-api"
 bucket="gw-deploy-$region"
 
 # Create deployment bucket if it doesn't exist
--- 3.gw-broker/sam.sh
+++ 3.gw-broker/sam.sh
@@ -2,7 +2,7 @@
 
 source `dirname "../bash/$BASH_SOURCE"`/getopts.sh
 
-stack="gw-http"
+stack="gw-broker"
 bucket="gw-deploy-$region"
 
 # Create deployment bucket if it doesn't exist
